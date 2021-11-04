bdc9eb2933f864ad5ec7571cb2450e7a4c7b9a88
Hernán Alperin <hernan.alperin@gmail.com>
2021-11-03 21:55:02 -0300

    borra cosa de MySQL

--- estilo_segmento/segmentos_color.sql
+++ estilo_segmento/segmentos_color.sql
@@ -1,22 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 02-11-2021 a las 18:48:01
--- Versión del servidor: 10.4.21-MariaDB
--- Versión de PHP: 8.0.12
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
+begin;
 --
 -- Base de datos: segmentos_color
 --
@@ -81,7 +63,3 @@ INSERT INTO segmentos_color (segmento, line_color, color, outile_color) VALUES
 ('39', '118,228,171,255', '255,255,255,255', '0,0,0,255'),
 ('40', '255,255,255,255', '', '');
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
